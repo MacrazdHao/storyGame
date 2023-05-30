403662efba96de6ec9934611701e983db5187596
unknown <gliu6@heygears.com>
2023-05-30 14:01:33 +0800

    sudoku

--- src/views/Sudoku.vue
+++ src/views/Sudoku.vue
@@ -1,75 +1,104 @@
 <template>
   <div class="Sudoku">
-    <p @click="initData3">生成矩阵</p>
-    <div class="rowsBox">
-      <div
-        v-for="(row, rowIndex) in rows"
-        :key="`row-${rowIndex}`"
-        class="rowsBox-item"
-      >
-        <p v-for="(item, index) in row" :key="`rowItem-${index}`">{{ item }}</p>
+    <p @click="resetMatrix">重置矩阵</p>
+    <p @click="crackMatrix">破解矩阵</p>
+    <p @click="getMatrix">生成完整矩阵</p>
+    <div class="matrixsBox">
+      <div class="rowsBox">
+        <div
+          v-for="(row, rowIndex) in rows"
+          :key="`row-${rowIndex}`"
+          class="rowsBox-item"
+        >
+          <p
+            v-for="(item, index) in row"
+            :key="`rowItem-${index}`"
+            class="rowsBox-item-num"
+          >
+            {{ item || "" }}
+          </p>
+        </div>
+      </div>
+      <div class="rowsBox">
+        <div
+          v-for="(row, rowIndex) in results[0]"
+          :key="`trow-${rowIndex}`"
+          class="rowsBox-item"
+        >
+          <input
+            v-for="(item, index) in row"
+            :key="`trowItem-${index}`"
+            :class="[
+              'rowsBox-item-num',
+              trows[rowIndex][index] ? 'rowsBox-item-num--static' : '',
+            ]"
+            :value="item || ''"
+            @input="(e) => inputNum(e.target.value, rowIndex, index)"
+          >
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-const trows = [
-  [0, 0, 0, 0, 5, 0, 0, 0, 0],
-  [0, 0, 0, 2, 0, 1, 3, 4, 0],
-  [9, 7, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 4, 0, 0, 7],
-  [0, 4, 0, 0, 0, 6, 0, 3, 1],
-  [3, 0, 0, 0, 0, 0, 0, 0, 0],
-  [6, 0, 0, 0, 0, 0, 0, 9, 0],
-  [0, 8, 0, 0, 1, 0, 0, 7, 0],
-  [2, 0, 3, 0, 8, 0, 0, 0, 6]
-]
-// const trows = [
-//   [0, 0, 0, 0, 5, 0, 0, 0, 0],
-//   [0, 0, 0, 2, 0, 1, 3, 4, 0],
-//   [9, 7, 0, 0, 0, 0, 0, 0, 0],
-//   [0, 0, 0, 0, 0, 0, 0, 0, 0],
-//   [0, 0, 0, 0, 0, 0, 0, 0, 0],
-//   [0, 0, 0, 0, 0, 0, 0, 0, 0],
-//   [0, 0, 0, 0, 0, 0, 0, 0, 0],
-//   [0, 0, 0, 0, 0, 0, 0, 0, 0],
-//   [0, 0, 0, 0, 0, 0, 0, 0, 0]
-// ]
-const tcols = [
-  [0, 0, 9, 0, 0, 3, 6, 0, 2],
-  [0, 0, 7, 0, 4, 0, 0, 8, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 3],
-  [0, 2, 0, 0, 0, 0, 0, 0, 0],
-  [5, 0, 0, 0, 0, 0, 0, 1, 8],
-  [0, 1, 0, 4, 6, 0, 0, 0, 0],
-  [0, 3, 0, 0, 0, 0, 0, 0, 0],
-  [0, 4, 0, 0, 3, 0, 9, 7, 0],
-  [0, 0, 0, 7, 1, 0, 0, 0, 6]
-]
-const tboxs = [
-  [0, 0, 0, 0, 0, 0, 9, 7, 0],
-  [0, 5, 0, 2, 0, 1, 0, 0, 0],
-  [0, 0, 0, 3, 4, 0, 0, 0, 0],
-  [0, 0, 0, 0, 4, 0, 3, 0, 0],
-  [0, 0, 4, 0, 0, 6, 0, 0, 0],
-  [0, 0, 7, 0, 3, 1, 0, 0, 0],
-  [6, 0, 0, 0, 8, 0, 2, 0, 3],
-  [0, 0, 0, 0, 1, 0, 0, 8, 0],
-  [0, 9, 0, 0, 7, 0, 0, 0, 6]
+const getDefaultMatrix = () => [
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0]
 ]
 export default {
   data() {
     return {
       level: 3,
-      rows: [],
+      rows: getDefaultMatrix(),
       columns: [],
       boxs: [],
       nums: [],
-      step: 0
+      step: 0,
+      trows: getDefaultMatrix(),
+      results: [getDefaultMatrix()]
     }
   },
-  computed: {},
+  computed: {
+    MatrixWidth() {
+      return this.level * this.level
+    },
+    tcols() {
+      return new Array(this.MatrixWidth).fill(null).map((item, c) => {
+        const col = []
+        for (let r = 0; r < this.trows.length; r++) {
+          col.push(this.trows[r][c])
+        }
+        return col
+      })
+    },
+    tboxs() {
+      return new Array(this.MatrixWidth).fill(null).map((item, b) => {
+        const box = []
+        for (
+          let r = Math.floor(b / this.level) * this.level;
+          r < Math.floor(b / this.level) * this.level + this.level;
+          r++
+        ) {
+          for (
+            let c = (b % this.level) * this.level;
+            c < (b % this.level) * this.level + this.level;
+            c++
+          ) {
+            box.push(this.trows[r][c])
+          }
+        }
+        return box
+      })
+    }
+  },
   mounted() {
     // 基础数字
     this.nums = new Array(this.level * this.level)
@@ -77,8 +106,12 @@ export default {
       .map((item, index) => index + 1)
   },
   methods: {
+    resetMatrix() {
+      this.results = [getDefaultMatrix()]
+    },
     // 算法方式
-    initData() {
+    getMatrix() {
+      this.rows = []
       const rows = new Array(this.level * this.level).fill(0).map(() => [])
       const cols = new Array(this.level * this.level).fill(0).map(() => [])
       const boxs = new Array(this.level * this.level).fill(0).map(() => [])
@@ -153,7 +186,8 @@ export default {
       this.boxs = boxs
     },
     // 随机数形式
-    initData2() {
+    getMatrix2() {
+      this.rows = []
       const rows = new Array(this.level * this.level)
         .fill(null)
         .map(() => new Set())
@@ -192,7 +226,7 @@ export default {
       }
       for (let i = 0; i < rows.length; i++) {
         if ([...rows[i]].length < 9) {
-          this.initData2()
+          this.getMatrix2()
           return
         }
       }
@@ -200,139 +234,55 @@ export default {
       this.columns = cols
       this.boxs = boxs
     },
-    // 随机数形式
-    initData4() {
-      const rows = new Array(this.level * this.level)
-        .fill(null)
-        .map(() => new Set())
-      const cols = new Array(this.level * this.level)
-        .fill(null)
-        .map(() => new Set())
-      const boxs = new Array(this.level * this.level)
-        .fill(null)
-        .map(() => new Set())
-      for (
-        let numRowIndex = 0;
-        numRowIndex < this.level * this.level;
-        numRowIndex++
-      ) {
-        for (
-          let numColIndex = 0;
-          numColIndex < this.level * this.level;
-          numColIndex++
-        ) {
-          // box的序号
-          const numBoxIndex =
-            Math.floor(numRowIndex / this.level) * this.level +
-            Math.floor(numColIndex / this.level)
-          const canUseNum = this.getDiffNums(
-            rows[numRowIndex],
-            cols[numColIndex],
-            boxs[numBoxIndex]
-          )
-          if (canUseNum.length === 0) break
-          const staticNum = trows[numRowIndex][rows[numRowIndex].size]
-          // console.log(numRowIndex, rows[numRowIndex].size, staticNum)
-          if (staticNum && canUseNum.includes(staticNum)) {
-            rows[numRowIndex].add(staticNum)
-            cols[numColIndex].add(staticNum)
-            boxs[numBoxIndex].add(staticNum)
-            continue
-          } else if (staticNum && !canUseNum.includes(staticNum)) break
-          const RanIndex = Math.floor(Math.random() * canUseNum.length)
-          const num = canUseNum[RanIndex]
-          rows[numRowIndex].add(num)
-          cols[numColIndex].add(num)
-          boxs[numBoxIndex].add(num)
-        }
+    // 破解方法
+    getAnswers(
+      r = this.trows,
+      c = this.tcols,
+      b = this.tboxs,
+      numRowIndex = 0,
+      numColIndex = 0
+    ) {
+      if (numRowIndex === this.MatrixWidth) {
+        this.results.push(r)
+        return r
       }
-      for (let i = 0; i < rows.length; i++) {
-        if ([...rows[i]].length < 9) {
-          this.initData3()
-          return
-        }
+      const rows = JSON.parse(JSON.stringify(r))
+      const cols = JSON.parse(JSON.stringify(c))
+      const boxs = JSON.parse(JSON.stringify(b))
+      // box的序号
+      const numBoxIndex =
+        Math.floor(numRowIndex / this.level) * this.level +
+        Math.floor(numColIndex / this.level)
+
+      const rowIndex = numColIndex
+      const colIndex = numRowIndex
+      const boxIndex =
+        (numRowIndex % this.level) * this.level + (numColIndex % this.level)
+      const canUseNum = this.trows[numRowIndex][numColIndex]
+        ? [this.trows[numRowIndex][numColIndex]]
+        : this.getDiffNums(
+          rows[numRowIndex],
+          cols[numColIndex],
+          boxs[numBoxIndex]
+        )
+      if (canUseNum.length === 0) return false
+      for (let i = 0; i < canUseNum.length; i++) {
+        rows[numRowIndex][rowIndex] = canUseNum[i]
+        cols[numColIndex][colIndex] = canUseNum[i]
+        boxs[numBoxIndex][boxIndex] = canUseNum[i]
+        if (numColIndex + 1 < this.MatrixWidth) {
+          this.getAnswers(rows, cols, boxs, numRowIndex, numColIndex + 1)
+        } else this.getAnswers(rows, cols, boxs, numRowIndex + 1, 0)
       }
-      this.rows = rows
-      this.columns = cols
-      this.boxs = boxs
+      return false
     },
-    initData3() {
-      const rows = new Array(this.level * this.level).fill(0).map(() => [])
-      const cols = new Array(this.level * this.level).fill(0).map(() => [])
-      const boxs = new Array(this.level * this.level).fill(0).map(() => [])
-      const CreateRecord = []
-      const CreateIndexRecord = new Array(
-        this.level * this.level * this.level * this.level
-      )
-        .fill(0)
-        .map(() => -1)
-      let numRowIndex = 0
-      let numColIndex = 0
-      while (numRowIndex < rows.length) {
-        while (numColIndex < cols.length) {
-          const step = numColIndex + numRowIndex * (this.level * this.level)
-          CreateIndexRecord[step]++
-          const recordIndex = CreateIndexRecord[step]
-          CreateRecord.length = step + 1
-          // box的序号
-          const numBoxIndex =
-            Math.floor(numRowIndex / this.level) * this.level +
-            Math.floor(numColIndex / this.level)
-          const rowIndex =
-            step + 1 - numRowIndex * (this.level * this.level) - 1
-          const colIndex = numRowIndex
-          const boxIndex =
-            (numRowIndex % this.level) * this.level +
-            (numColIndex % this.level)
-          let canUseNum =
-            CreateRecord[step] ||
-            this.getDiffNums(
-              rows[numRowIndex],
-              cols[numColIndex],
-              boxs[numBoxIndex]
-            )
-          const staticNum = trows[numRowIndex][rowIndex]
-          const staticIndex = canUseNum.indexOf(staticNum)
-          if (staticNum && staticIndex > -1 && !CreateRecord[step]) {
-            const tmp = canUseNum[staticIndex]
-            canUseNum[staticIndex] = staticNum
-            canUseNum[0] = tmp
-          }
-          this.step = step
-          if (canUseNum.length === 0 || recordIndex >= canUseNum.length) {
-            rows[numRowIndex].length = rowIndex
-            cols[numColIndex].length = colIndex
-            boxs[numBoxIndex].length = boxIndex
-            CreateIndexRecord[step] = -1
-            // 可用数字用尽  则  返回上一步
-            if (numColIndex === 0) {
-              // 列序为0，则返回到上一行的最后一列
-              numRowIndex -= 2 // 这里-2是因为+1会在后面执行
-              numColIndex = this.level * this.level - 1
-              break
-            }
-            if (numColIndex > 0) {
-              numColIndex -= 1 // 这里-1是因为后面的+1不会执行
-              continue
-            }
-          }
-          if (step >= CreateRecord.length) {
-            CreateRecord.push(canUseNum)
-          } else {
-            CreateRecord[step] = canUseNum
-          }
-          const num = canUseNum[recordIndex]
-          rows[numRowIndex][rowIndex] = num
-          cols[numColIndex][colIndex] = num
-          boxs[numBoxIndex][boxIndex] = num
-          numColIndex++
-        }
-        if (numColIndex >= cols.length) numColIndex = 0
-        numRowIndex++
-      }
-      this.rows = rows
-      this.columns = cols
-      this.boxs = boxs
+    // 执行矩阵破解
+    crackMatrix() {
+      // if ()
+      this.hasStaticNum = true
+      this.results = []
+      this.getAnswers()
+      console.log(this.results)
     },
     getDiffNums(a, b, c) {
       // a∪b∪c
@@ -343,6 +293,16 @@ export default {
         .sort(() => {
           return 0.5 - Math.random()
         })
+    },
+    inputNum(val, r, c) {
+      this.$set(
+        this.trows,
+        r,
+        this.trows[r].map((oval, idx) => {
+          return idx === c ? val : oval
+        })
+      )
+      this.results = [JSON.parse(JSON.stringify(this.trows))]
     }
   }
 }
@@ -354,19 +314,46 @@ export default {
   height: 100%;
   display: flex;
   flex-direction: column;
+  input {
+    background: none;
+    outline: none;
+    border: none;
+    margin: 0;
+  }
+  .matrixsBox {
+    width: 100%;
+    display: flex;
+    flex-direction: row;
+  }
+  .rowsBox + .rowsBox {
+    margin-left: 12px;
+  }
   .rowsBox {
     width: fit-content;
     height: fit-content;
     display: flex;
     flex-direction: column;
+    border-right: 1px solid #000;
+    border-top: 1px solid #000;
     &-item {
       width: fit-content;
       display: flex;
       flex-direction: row;
-      p {
-        width: 20px;
-        height: 20px;
-        line-height: 20px;
+      &-num {
+        width: 30px;
+        height: 30px;
+        font-size: 16px;
+        line-height: 32px;
+        text-align: center;
+        border-left: 1px solid #000;
+        border-bottom: 1px solid #000;
+      }
+      &-num--static {
+        font-weight: bold;
+        // color: rgb(93, 65, 0);
+        // font-size: 14px;
+        // text-shadow: 0 0 10px rgb(93, 65, 0);
+        background-color: rgb(200, 200, 200);
       }
     }
   }
